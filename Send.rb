bcdc83757e55c197c44ff4854350da447464d088
Guanting Chen <carpo98@gmail.com>
2012-01-20 09:11:47 +0800

    Fix debug feature

--- Send.rb
+++ Send.rb
@@ -19,7 +19,13 @@ class Send
 			request = Net::HTTP::Post.new(uri.request_uri, 'User-Agent' => options[:useragent])
 			request.set_form_data(options[:data])
 			@@last_response = http.request(request)
-			return @@last_response.body
+
+			if '200' == @@last_response.code then
+				return @@last_response.body
+			else
+				@@last_exception = @@last_response.code
+				return nil
+			end
 		rescue Exception => e
 			@@last_exception = e
 			return nil
@@ -27,14 +33,23 @@ class Send
 	end
 
 	def self.error
-		return @@last_exception.message
+		if @@last_exception.kind_of?(String) then
+			return @@last_exception
+		else
+			return @@last_exception.message
+		end
 	end
 
 	def self.debug
-		return @@last_exception.backtrace.inspect
+		if @@last_exception.kind_of?(String) then
+			return 'HTTP_ERROR'
+		else
+			return @@last_exception.backtrace.inspect
+		end
 	end
 end
 
 # how to use
-# puts Send.request('http://google.com', data: { hello: 'world.' } )
-# puts Send.error
+response = Send.request('http://google.com', data: { hello: 'world.' } )
+response = Send.error if response.nil?
+puts response
